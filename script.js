12c6116a69a752d1af81452a4d6e116bdf735477
EddyJVC <eddy.villalobos.che@gmail.com>
2022-11-15 17:05:15 -0600

    clase03 completada

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const btn = document.querySelector("[data-form-btn]");
-const input = document.querySelector("[data-form-input]");
 
-// console.log(btn);
 const createTask = (e) => {
   e.preventDefault();
-  console.log("Crear Tarea");
-  console.log(input.value);
+  const input = document.querySelector("[data-form-input]");
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  const value = input.value;
   input.value = "";
+  const content = `
+  <div>
+  <i class="far fa-check-square icon"></i>
+  <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  task.innerHTML = content;
+
+  list.appendChild(task);
+  task.classList.add("card");
+  console.log(content);
 };
 
 //listener
