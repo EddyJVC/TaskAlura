459bb032df9c13c029b69b765cfc142842f02c2c
EddyJVC <eddy.villalobos.che@gmail.com>
2022-11-15 17:41:04 -0600

    clase04 completada

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-const btn = document.querySelector("[data-form-btn]");
+// Immediately invoked function expression IIFE - Mas Seguridad
+(() => {
+  const btn = document.querySelector("[data-form-btn]");
 
-const createTask = (e) => {
-  e.preventDefault();
-  const input = document.querySelector("[data-form-input]");
-  const list = document.querySelector("[data-list]");
-  const task = document.createElement("li");
-  const value = input.value;
-  input.value = "";
-  const content = `
-  <div>
-  <i class="far fa-check-square icon"></i>
-  <span class="task">${value}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-  task.innerHTML = content;
+  const createTask = (e) => {
+    e.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    const value = input.value;
+    input.value = "";
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    const trashTask = document.createElement("i");
+    trashTask.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
+    task.appendChild(taskContent);
+    task.appendChild(trashTask);
+    list.appendChild(task);
+  };
 
-  list.appendChild(task);
-  task.classList.add("card");
-  console.log(content);
-};
+  //listener
+  btn.addEventListener("click", createTask);
 
-//listener
-btn.addEventListener("click", createTask);
+  const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+  };
+
+  const completeTask = (e) => {
+    const element = e.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+  };
+})();
