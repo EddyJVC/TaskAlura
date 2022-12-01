5fb760575085d4278cc59112af6b7067f96ef205
EddyJVC <eddy.villalobos.che@gmail.com>
2022-12-01 17:55:46 -0600

    Agregando Local Storage

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-// Immediately invoked function expression IIFE - Mas Seguridad
-import checkComplete from "./components/checkComplete.js";
-import deleteIcon from "./components/deleteIcon.js";
+import { addTask } from "./components/addTask.js";
+import { displayTasks } from "./components/displayTasks.js";
 
+// Immediately invoked function expression IIFE - Mas Seguridad
 (() => {
   const btn = document.querySelector("[data-form-btn]");
 
-  const createTask = (e) => {
-    e.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card");
-    const value = input.value;
-    input.value = "";
-    const taskContent = document.createElement("div");
-    taskContent.appendChild(checkComplete());
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(titleTask);
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
-    list.appendChild(task);
-  };
-
   //listener
-  btn.addEventListener("click", createTask);
+  btn.addEventListener("click", addTask);
+  displayTasks();
 })();
